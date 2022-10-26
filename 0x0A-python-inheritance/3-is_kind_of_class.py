ff3e6a5c45535b089ef494df31d21623d063cf19
haydernu <74455355+haydernu@users.noreply.github.com>
2022-10-26 22:47:37 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-# 3-is_kind_of_class.py
-"""Defines a class and inherited class-checking function."""
+# 5-to_json_string.py
+"""Defines a string-to-JSON function."""
+import json
 
 
-def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance or inherited instance of a class.
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an instance or inherited instance of a_class - True.
-        Otherwise - False.
-    """
-    if isinstance(obj, a_class):
-        return True
-    return False
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
