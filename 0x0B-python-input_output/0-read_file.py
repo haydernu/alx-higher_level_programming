71850b42deafbe66faae4e373108243b9b19bf1d
haydernu <74455355+haydernu@users.noreply.github.com>
2022-10-26 23:02:57 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,8 +7,3 @@ def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privac
