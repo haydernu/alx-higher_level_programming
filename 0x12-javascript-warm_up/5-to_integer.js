9f36ae57e9e76d2feb2703f39ad6d9198e390b9e
Hayder <haydernuredin2212@gmail.com>
2022-12-21 05:30:24 -0800

    new5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+
+const argsToConvert = parseInt(process.argv[2]);
+if (isNaN(argsToConvert)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + argsToConvert);
+}
