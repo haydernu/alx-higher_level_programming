d699f32c8495e6a9c8ecd83a9108fcdc864ad066
Hayder <haydernuredin2212@gmail.com>
2022-12-23 04:48:46 -0800

    new 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-
 const list = require('./100-data').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
 console.log(list);
-const newList = list.map((v, i) => v * i);
 console.log(newList);
