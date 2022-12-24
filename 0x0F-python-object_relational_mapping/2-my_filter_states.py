767483e26ae150ca51c773a5f822731bbf9922a3
Hayder <haydernuredin2212@gmail.com>
2022-12-24 07:22:00 -0800

    one new

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument
+Lists all values in the states tables of a database where name
+matches the argument
 """
-from sys import argv
+
+import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
-    user, password, database, state = argv[1], argv[2], argv[3], argv[4]
-    db = MySQLdb.connect(host="localhost",
-                         user=user, passwd=password, db=database)
-    db = db.cursor()
-    db.execute("""SELECT * FROM states
-    WHERE name LIKE BINARY '{}' ORDER BY id"""
-               .format(state))
-    r = db.fetchall()
-    for i in r:
-        print(i)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
