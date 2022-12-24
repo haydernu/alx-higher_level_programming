72be53214193bfd0201389971e0ecb0597653257
Hayder <haydernuredin2212@gmail.com>
2022-12-24 06:52:45 -0800

    new one

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import MySQLdb
 from sys import argv
 
@@ -8,6 +7,7 @@ from sys import argv
 a script that lists all states
 from the database
 '''
+
 if __name__ == "__main__":
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
