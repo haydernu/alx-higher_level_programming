b39125fd71fa0e0069fd93d5422f86363aa1c865
Hayder <haydernuredin2212@gmail.com>
2022-12-24 07:04:30 -0800

    new 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(host="localhost", user=user,
                          passwd=password, db=database)
+
     db = db.cursor()
     db.execute("""SELECT * FROM states
     WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
