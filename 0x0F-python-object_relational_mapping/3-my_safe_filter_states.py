ae0a064980966bacb8629b1460d87f8b452c555b
Hayder <haydernuredin2212@gmail.com>
2022-12-24 11:08:56 -0800

    new 34

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""SQL injection"""
-from sys import argv
+"""
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
+"""
+
+import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
-    user, password, database, state = argv[1], argv[2], argv[3], argv[4]
-    db = MySQLdb.connect(host="localhost",
-                         user=user, passwd=password, db=database)
-    db = db.cursor()
-    db.execute("""SELECT * FROM states WHERE name=%s ORDER BY id""", (state,))
-    r = db.fetchall()
-    for i in r:
-        print(i)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""script that lists all cities from the database hbtn_0e_4_usa
 """
-from sys import argv
+List all cities from a database
+"""
+
+import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
-    user, password, database = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=password, db=database)
-    db = db.cursor()
-    db.execute("""SELECT cities.id, cities.name, states.name FROM cities
-    JOIN states ON state_id=states.id ORDER BY cities.id""")
-    r = db.fetchall()
-    for i in r:
-        print(i)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
