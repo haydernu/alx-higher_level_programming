53c403407b9828ea43ed485af16f0d3a5a49e92a
Hayder <haydernuredin2212@gmail.com>
2022-12-24 07:09:21 -0800

    one new

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa
 """
-from sys import argv
+Lists all states with a name starting with N
+"""
+import sys
 import MySQLdb
 
-
 if __name__ == '__main__':
-    user, password, database = argv[1], argv[2], argv[3]
-    db = MySQLdb.connect(host="localhost", user=user,
-                         passwd=password, db=database)
-
-    db = db.cursor()
-    db.execute("""SELECT * FROM states
-    WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
-    r = db.fetchall()
-    for i in r:
-        print(i)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
