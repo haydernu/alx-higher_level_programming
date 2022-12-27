4a8f50556de8c79abaf6dee9200d9638086174e6
Hayder <haydernuredin2212@gmail.com>
2022-12-27 04:59:38 -0800

    fixed

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -15,18 +15,17 @@ if __name__ == "__main__":
     Access to the database and get the cities
     from the database.
     """
-
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
+    
     session = Session()
     cal_state = State(name='California')
     sfr_city = City(name='San Francisco')
     cal_state.cities.append(sfr_city)
-    
+
     session.add(cal_state)
     session.commit()
     session.close()
